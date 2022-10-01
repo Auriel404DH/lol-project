a95741ad862f34eb699d5028b12ed47592f2920a
Vladislav Rykov <vladislav.ok.07@mail.ru>
2022-10-01 15:29:01 +0300

    test

--- src/pages/mainP/MainPage.tsx
+++ src/pages/mainP/MainPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './MainPage.module.scss';
 
 const MainPage = () => {
-  return <div className="text-main">Main</div>;
+  return <div className="text-main">Main test</div>;
 };
 
 export default MainPage;
