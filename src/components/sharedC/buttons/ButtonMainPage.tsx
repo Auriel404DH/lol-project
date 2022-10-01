2fdb0a048b2bf369fea877782459101d839311c0
Vladislav Rykov <vladislav.ok.07@mail.ru>
2022-10-01 21:03:06 +0300

    пофиксил кнопку

--- src/components/sharedC/buttons/ButtonMainPage.tsx
+++ src/components/sharedC/buttons/ButtonMainPage.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './ButtonMainPage.module.scss'
 
-const ff = ['top','right','bottom','left']
-const ButtonMainPage = () => {
+
+
+
+const ButtonMainPage = ({value}:{value:string}) => {
+
+  const ff = ['top','right','bottom','left']
+
   return (
     <div className={styles.wrapper}>
       <button className={`${styles.wrapper__btn} ${styles.btn}`}> 
         {ff.map((el,idx) => <span key={idx} className={`${styles.btn__line} ${styles["btn__line--"+el]}`} ></span>)}
-        Нажми сюда
+        {value}
       </button>
     </div>
   );
--- src/pages/testP/TestPage.tsx
+++ src/pages/testP/TestPage.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ButtonMainPage from '../../components/sharedC/buttons/ButtonMainPage'
 
 const TestPage = () => {
+
+  const str:string = 'Далее'
+
+
   return (
     <div className='ml-12'>
       <div>TestPage</div>
-      <ButtonMainPage/>
+      <ButtonMainPage value={str}/>
     </div>
 
   
